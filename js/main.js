71020dee48f3f245aa8512f952928877f7c23232
MIL-KIL-GIT-938 <166957334+MIL-KIL-GIT-938@users.noreply.github.com>
2024-05-19 06:37:11 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var result = document.getElementById('result');
 var number = 0;
+var Timer = 0;
+var Timer_2 = 4;
 var aut = "";
+var clickCounter = 0; // Объявляем переменную clickCounter и инициализируем ее значением 0
+// Получаем элемент по его идентификатору
+var element = document.getElementById('result_2');
+var result_3 = document.getElementById('result_3');
 
 function onTelegramAuth(user) {
     aut = user.first_name;
@@ -16,7 +22,18 @@ function OnClickButton() {
     if (aut === '') {
         alert('Авторизуйтесь! Через телеграмм!');
     } else {
-        number++;
-        result.innerHTML = 'Ты нажал(а) ' + number + ' раз';
+        clickCounter++; // Увеличиваем значение переменной clickCounter на 1
+        result.innerHTML = 'Ты нажал(а) ' + clickCounter + ' раз'; // Обновляем содержимое элемента с id "result"
+
+        clearInterval(Timer); // Останавливаем таймер, если он был запущен ранее
+
+        Timer = setInterval(function () {
+            clearInterval(Timer); // Останавливаем таймер после 3 секунд
+            console.log("Таймер завершен");
+            var resetBtn = document.getElementById("bt"); // Получаем кнопку с id "bt"
+            resetBtn.disabled = true; // Делаем кнопку неактивной
+            element.style.display = 'block';
+            result_3.innerHTML = 'Ты нажал(а) ' + clickCounter + ' раз';
+        }, 3000); // Таймер на 3 секунды
     }
 }
