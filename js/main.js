96e33bc72c043a58b72d21f4a6b54ff25cb4c42a
MIL-KIL-GIT-938 <166957334+MIL-KIL-GIT-938@users.noreply.github.com>
2024-05-19 04:48:41 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 function onTelegramAuth(user) {
     document.getElementById('hell_2').innerHTML = user.first_name;
+    document.getElementById('hell').textContent = 'Привет, ' + user.first_name + '!';
+    aut = 'Привет, ' + user.first_name + '!';
 }
 
 var result = document.getElementById('result');
 var number = 0;
-var aut = document.getElementById('hell').textContent
+var aut = document.getElementById('hell').textContent;
 
 function OnClickButton() {
-
-    if (aut == 'Добро пожаловать незнакомец!') {
-        alert('Авторизуйтесь! Через телеграмм!')
+    if (aut === 'Добро пожаловать незнакомец!' || aut === '') {
+        alert('Авторизуйтесь! Через телеграмм!');
     } else {
         number++;
         result.innerHTML = 'Ты нажал(а) ' + number + ' раз';
